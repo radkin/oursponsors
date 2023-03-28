2c711d58fb57406fd16fcec4f8fbe7c6ac48f8ea
Noel Miller <noeldamonmiller@gmail.com>
2023-03-27 17:30:52 -0700

    async_storage_for_prefs
    switched to redux. Set a shell for setting preferences that does not yet
    impact preferences in the back end. it will not survive app restart

--- src/components/CustomDrawerContent.tsx
+++ src/components/CustomDrawerContent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Drawer} from 'react-native-paper';
 import {
   DrawerContentScrollView,
@@ -6,23 +6,88 @@ import {
   DrawerItemList,
 } from '@react-navigation/drawer';
 function CustomDrawerContent(props) {
-  const [active, setActive] = React.useState('');
+  // General Preferences
+  const [myStateOnly, setCheck] = useState(true);
+  const [myPartyOnly, setMyPartyOnly] = useState(false);
+  // Hide Detail Preferences
+  const [twitter, setHideTwitter] = useState(false);
+  const [facebook, setHideFacebook] = useState(false);
+  const [youtube, setHideYoutube] = useState(false);
+  const [googleEntity, setHideGoogleEntity] = useState(false);
+  const [cspan, setHideCspan] = useState(false);
+  const [voteSmart, setHideVoteSmart] = useState(false);
+  const [govTrack, setHideGovTrack] = useState(false);
+  const [openSecrets, setHideOpenSecrets] = useState(false);
+
+  const showMyStateOnly = () => {setCheck(prevState => !prevState);};
+  const showMyPartyOnly = () => {setMyPartyOnly(prevState => !prevState);};
+  const hideTwitter = () => {setHideTwitter(prevState => !prevState);};
+  const hideFacebook = () => {setHideTwitter(prevState => !prevState);};
+  const hideYouTube = () => {setHideYoutube(prevState => !prevState);};
+  const hideGoogleEntity = () => {setHideGoogleEntity(prevState => !prevState);};
+  const hideCspan = () => {setHideCspan(prevState => !prevState);};
+  const hideVoteSmart = () => {setHideVoteSmart(prevState => !prevState);};
+  const hideGovTrack = () => {setHideGovTrack(prevState => !prevState);};
+  const hideOpenSecrets = () => {setHideOpenSecrets(prevState => !prevState);};
 
   return (
     <DrawerContentScrollView {...props}>
       <DrawerItemList {...props} />
-      <Drawer.Section title="Some title">
+      <Drawer.Section title="General Preferences">
         <Drawer.Item
-          label="Filter for state of CA"
-          active={active === 'first'}
-          onPress={() => setActive('first')}
+          label="only California"
+          active={myStateOnly === true}
+          onPress={() => showMyStateOnly()}
         />
         <Drawer.Item
-          label="Show All"
-          active={active === 'second'}
-          onPress={() => setActive('second')}
+          label="only Democratics"
+          active={myPartyOnly === true}
+          onPress={() => showMyPartyOnly()}
         />
       </Drawer.Section>
+      <Drawer.Section title="Hide Details Preferences">
+        <Drawer.Item
+          label="twitter"
+          active={twitter === true}
+          onPress={() => hideTwitter()}
+        />
+        <Drawer.Item
+          label="facebook"
+          active={facebook === true}
+          onPress={() => hideFacebook()}
+        />
+        <Drawer.Item
+          label="youTube"
+          active={youtube === true}
+          onPress={() => hideYouTube()}
+        />
+        <Drawer.Item
+          label="google entity"
+          active={googleEntity === true}
+          onPress={() => hideGoogleEntity()}
+        />
+        <Drawer.Item
+          label="cspan"
+          active={cspan === true}
+          onPress={() => hideCspan()}
+        />
+        <Drawer.Item
+          label="vote smart"
+          active={voteSmart === true}
+          onPress={() => hideVoteSmart()}
+        />
+        <Drawer.Item
+          label="gov track"
+          active={govTrack === true}
+          onPress={() => hideGovTrack()}
+        />
+        <Drawer.Item
+          label="open secrets"
+          active={openSecrets === true}
+          onPress={() => hideOpenSecrets()}
+        />
+      </Drawer.Section>
+
       <DrawerItem label="Help" onPress={() => alert('Link to help')} />
     </DrawerContentScrollView>
   );
