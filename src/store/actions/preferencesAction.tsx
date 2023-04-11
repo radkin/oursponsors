7448ee4ac34b1dd2f46b7e55345355d370011344
Noel Miller <noeldamonmiller@gmail.com>
2023-04-11 16:35:10 -0700

    preferences_view we don't need the preference id to make changes. This depends entirely on the user_id. At least it should

--- src/store/actions/preferencesAction.tsx
+++ src/store/actions/preferencesAction.tsx
@@ -25,7 +25,6 @@ export const getPreferences = () => async dispatch => {
 
 export const updatePreferences = (pref, value) => async dispatch => {
   const prefData = {
-    id: 1,
     user_id: 1,
     [pref]: value,
   };
