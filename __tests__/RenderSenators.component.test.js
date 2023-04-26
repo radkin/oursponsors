c25be07ad2efe91e6b2421235d53c9a2fde35c09
Noel Miller <noeldamonmiller@gmail.com>
2023-04-25 18:19:43 -0700

    jest_starter this is what is needed to get jest to work

---
+++ __tests__/RenderSenators.component.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import RenderSenators from '../src/components/RenderSenators';
+
+describe('RenderSenators', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <RenderSenators/>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
