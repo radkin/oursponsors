a7aebb6a573d1ae3c14a9179d7658fb411c916f3
Noel Miller <noeldamonmiller@gmail.com>
2023-05-29 15:47:09 -0700

    user_profile removed formbuilder

--- src/components/screens/ProfileScreen.tsx
+++ src/components/screens/ProfileScreen.tsx
@@ -1,295 +1,195 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {View, StyleSheet, ScrollView} from 'react-native';
-import {useController, useForm} from 'react-hook-form';
-import {Button, Checkbox, List, TextInput} from 'react-native-paper';
-import {FormBuilder} from 'react-native-paper-form-builder';
-import {LogicProps} from 'react-native-paper-form-builder/dist/Types/Types';
+import {
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  Text,
+  View,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+
 import {useAppDispatch, useAppSelector} from '../../hooks';
 import {connect} from 'react-redux';
 import {getUser} from '../../store/actions/userAction';
+import {useForm, Controller} from 'react-hook-form';
+// import { TextInput } from "react-native-paper";
+
+const listData = [
+  {label: 'Male', value: 'male'},
+  {label: 'Female', value: 'female'},
+];
 
 function ProfileScreen({value}) {
   const dispatch = useAppDispatch();
   const userObjectData = useAppSelector(state => state.userObject);
   const {user} = userObjectData;
 
-  const [internalState, setInternalState] = useState(value);
-  // internal state for all user properties
-  const [firstName, setFirstName] = useState(user.first_name);
-  const [lastName, setLastName] = useState(user.last_name);
-  const [email, setEmail] = useState(user.email);
+  useEffect(() => {
+    dispatch(getUser());
+  }, [dispatch]);
 
+  // sync state
+  const [internalState, setInternalState] = useState(value);
   const previousValueRef = useRef();
   const previousValue = previousValueRef.current;
   if (value !== previousValue && value !== internalState) {
     setInternalState(value);
-    setFirstName(value.user.first_name);
-    setLastName(value.user.last_name);
-    setEmail(value.email);
   }
-
   useEffect(() => {
     previousValueRef.current = value;
     dispatch(getUser());
   }, [dispatch]);
 
-  console.log(user);
-
-  // const firstName = 'Joe';
-
-  const {control, setFocus, handleSubmit} = useForm({
-    defaultValues: {
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      password: '',
-      state: '',
-      gender: '',
-      party: '',
-      rememberMe: 'checked',
-    },
-    mode: 'onChange',
-  });
-
-  return (
-    <View style={styles.containerStyle}>
-      {user && (
-        <ScrollView contentContainerStyle={styles.scrollViewStyle}>
-          <FormBuilder
-            control={control}
-            setFocus={setFocus}
-            formConfigArray={[
-              [
-                {
-                  name: 'firstName',
-                  type: 'text',
-                  textInputProps: {
-                    label: 'First Name',
-                    left: <TextInput.Icon name={'account'} />,
-                  },
-                  rules: {
-                    required: {
-                      value: true,
-                      message: 'First name is required',
-                    },
-                  },
-                  flex: 1.5,
-                },
-                {
-                  name: 'lastName',
-                  type: 'text',
-                  textInputProps: {
-                    label: 'Last Name',
-                    left: <TextInput.Icon name={'account'} />,
-                  },
-                  rules: {
-                    required: {
-                      value: true,
-                      message: 'Last name is required',
-                    },
-                  },
-                  flex: 1,
-                },
-              ],
-              {
-                name: 'email',
-                type: 'email',
-                textInputProps: {
-                  label: 'Email',
-                  left: <TextInput.Icon name={'email'} />,
-                },
-                rules: {
-                  required: {
-                    value: true,
-                    message: 'Email is required',
-                  },
-                  pattern: {
-                    value:
-                      /[A-Za-z0-9._%+-]{3,}@[a-zA-Z]{3,}([.]{1}[a-zA-Z]{2,}|[.]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,})/,
-                    message: 'Email is invalid',
-                  },
-                },
-              },
-              {
-                name: 'password',
-                type: 'password',
-                textInputProps: {
-                  label: 'Password',
-                  left: <TextInput.Icon name={'lock'} />,
-                },
-                rules: {
-                  required: {
-                    value: true,
-                    message: 'Password is required',
-                  },
-                  minLength: {
-                    value: 8,
-                    message: 'Password should be atleast 8 characters',
-                  },
-                  maxLength: {
-                    value: 30,
-                    message: 'Password should be between 8 and 30 characters',
-                  },
-                },
-              },
-              {
-                name: 'state',
-                type: 'autocomplete',
-                textInputProps: {
-                  label: 'State',
-                  left: <TextInput.Icon name={'office-building'} />,
-                },
-                rules: {
-                  required: {
-                    value: true,
-                    message: 'State is required',
-                  },
-                },
-                options: [
-                  {
-                    label: 'California',
-                    value: 1,
-                  },
-                  {
-                    label: 'Idaho',
-                    value: 2,
-                  },
-                  {
-                    label: 'Colorado',
-                    value: 3,
-                  },
-                  {
-                    label: 'Texas',
-                    value: 4,
-                  },
-                  {
-                    label: 'New York',
-                    value: 5,
-                  },
-                  {
-                    label: 'Utah',
-                    value: 6,
-                  },
-                  {
-                    label: 'Arizona',
-                    value: 7,
-                  },
-                  {
-                    label: 'Florida',
-                    value: 8,
-                  },
-                ],
-              },
-              {
-                name: 'gender',
-                type: 'select',
-                textInputProps: {
-                  label: 'Gender',
-                  left: <TextInput.Icon name={'account'} />,
-                },
-                rules: {
-                  required: {
-                    value: true,
-                    message: 'Gender is required',
-                  },
-                },
-                options: [
-                  {
-                    value: 0,
-                    label: 'Female',
-                  },
-                  {
-                    value: 1,
-                    label: 'Male',
-                  },
-                  {
-                    value: 2,
-                    label: 'Others',
-                  },
-                ],
-              },
-              {
-                name: 'party',
-                type: 'select',
-                textInputProps: {
-                  label: 'Party',
-                  left: <TextInput.Icon name={'account'} />,
-                },
-                rules: {
-                  required: {
-                    value: true,
-                    message: 'Party is required',
-                  },
-                },
-                options: [
-                  {
-                    value: 0,
-                    label: 'Independent',
-                  },
-                  {
-                    value: 1,
-                    label: 'Democrat',
-                  },
-                  {
-                    value: 2,
-                    label: 'Republican',
-                  },
-                ],
-              },
-              {
-                name: 'rememberMe',
-                type: 'custom',
-                JSX: TermsCheckBox,
-              },
-            ]}
-          />
-          <Button mode={'contained'} onPress={handleSubmit(console.log)}>
-            Submit
-          </Button>
-        </ScrollView>
-      )}
-    </View>
-  );
-}
-
-function TermsCheckBox(props: LogicProps) {
-  const {name, rules, shouldUnregister, defaultValue, control} = props;
-  const {field} = useController({
-    name,
-    rules,
-    shouldUnregister,
-    defaultValue,
+  const [listOpen, setListOpen] = useState(false);
+  const {
     control,
-  });
+    handleSubmit,
+    formState: {errors},
+  } = useForm();
 
   return (
-    <List.Item
-      title={'Remember me'}
-      left={() => (
-        <Checkbox.Android
-          status={field.value}
-          onPress={() => {
-            field.onChange(field.value === 'checked' ? 'unchecked' : 'checked');
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Controller
+          control={control}
+          name="name"
+          render={({field: {onChange, value}}) => (
+            <View style={styles.textBox}>
+              <TextInput
+                style={styles.text}
+                placeholder="Name"
+                defaultValue={value}
+                onChangeText={v => onChange(v)}
+              />
+            </View>
+          )}
+          rules={{
+            required: {
+              value: true,
+              message: 'Please fill out all required fields.',
+            },
           }}
         />
-      )}
-    />
+
+        {errors.name?.message ? (
+          <Text style={styles.errorText}>{errors.name?.message}</Text>
+        ) : null}
+
+        <Controller
+          control={control}
+          name="email"
+          render={({field: {onChange, value}}) => (
+            <View style={styles.textBox}>
+              <TextInput
+                style={styles.text}
+                placeholder="Email"
+                defaultValue={value}
+                onChangeText={v => onChange(v)}
+              />
+            </View>
+          )}
+          rules={{
+            required: {
+              value: true,
+              message: 'Please fill out all required fields.',
+            },
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: 'Please enter valid email.',
+            },
+          }}
+        />
+
+        {errors.email?.message ? (
+          <Text style={styles.errorText}>{errors.email?.message}</Text>
+        ) : null}
+
+        <Controller
+          control={control}
+          name="gender"
+          render={({field: {onChange, value}}) => (
+            <DropDownPicker
+              style={styles.dropdown}
+              placeholder="Select your gender"
+              placeholderStyle={styles.dropdownPlaceholder}
+              open={listOpen}
+              setOpen={() => setListOpen(!listOpen)}
+              items={listData}
+              value={value}
+              setValue={item => onChange(item())}
+            />
+          )}
+          rules={{
+            required: {
+              value: true,
+              message: 'Please fill out all required fields.',
+            },
+          }}
+        />
+
+        {errors.gender?.message ? (
+          <Text style={styles.errorText}>{errors.gender?.message}</Text>
+        ) : null}
+      </View>
+
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleSubmit(formValue =>
+            Alert.alert('Form Value', JSON.stringify(formValue)),
+          )}>
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  containerStyle: {
+  safeArea: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
   },
-  scrollViewStyle: {
-    flex: 1,
-    padding: 15,
-    justifyContent: 'center',
+  container: {
+    width: '90%',
   },
-  headingStyle: {
-    fontSize: 30,
-    textAlign: 'center',
-    marginBottom: 40,
+  textBox: {
+    backgroundColor: '#fafafa',
+    borderRadius: 10,
+    paddingStart: 5,
+    borderColor: '#e4e4e4',
+    borderWidth: 1,
+    alignSelf: 'stretch',
+    marginVertical: 7,
+  },
+  text: {
+    fontSize: 15,
+    paddingVertical: 10,
+  },
+  errorText: {
+    color: 'red',
+  },
+  dropdown: {
+    borderColor: '#e4e4e4',
+    backgroundColor: '#fafafa',
+    marginVertical: 10,
+  },
+  dropdownPlaceholder: {
+    color: '#c7c7c8',
+  },
+  button: {
+    width: '100%',
+    backgroundColor: '#517CFF',
+    borderRadius: 10,
+    paddingVertical: 10,
+    marginVertical: 20,
+  },
+  buttonText: {
+    color: 'white',
+    alignSelf: 'center',
   },
 });
 
