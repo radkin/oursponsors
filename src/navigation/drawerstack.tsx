c06eb17ae9583d25799dd3999ee67638cc41db5e
Noel Miller <noeldamonmiller@gmail.com>
2023-06-16 15:55:22 -0700

    firebase_login_auth logout ready. From here it's small improvements

--- src/navigation/drawerstack.tsx
+++ src/navigation/drawerstack.tsx
@@ -1,125 +1,137 @@
-import React, { FC, useState } from "react";
-import {
-  PreferencesScreen,
-  ProfileScreen,
-} from '../screens';
-import { createDrawerNavigator, DrawerContentScrollView } from "@react-navigation/drawer";
+import React, {FC, useState} from 'react';
+import {PreferencesScreen, ProfileScreen} from '../screens';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import {scale} from 'react-native-size-matters';
 import Tabstack from './tabstack';
-import login from "../screens/login";
-import { auth } from "../constants/firebase";
-import {View} from "react-native";
-import {Button} from "../components";
+import login from '../screens/login';
+import {auth} from '../constants/firebase';
+import {View, Button, Text} from 'react-native';
+import signup from "../screens/signup";
 
 const Drawer = createDrawerNavigator();
 
 const Drawerstack: FC = () => {
   const [logout, setLogout] = useState<number>(0);
-
-  const signOut = () => {
-    auth.signOut();
-    setLogout(1);
+  
+  const LogoutScreen = () => {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Button
+          title="Logout"
+          onPress={() => {
+            auth.signOut();
+            setLogout(1);
+          }}
+        />
+      </View>
+    );
   };
 
-  console.log(`logout is ${logout}`);
-
-  const CustomComponent = (props) => {
+  if (logout === 0) {
     return (
-      <DrawerContentScrollView {...props}>
-        <View>
-          <Button title="log out" onPress={signOut} />
-        </View>
-      </DrawerContentScrollView>
-    )
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: {
+            height: scale(25),
+          },
+        }}>
+        <Drawer.Screen
+          name="Summary"
+          component={Tabstack}
+          options={{
+            headerTitleStyle: {
+              fontSize: scale(13),
+              lineHeight: scale(13) * 0.75,
+              paddingTop: scale(13) - scale(13) * 0.75,
+            },
+            drawerLabelStyle: {
+              fontSize: scale(13),
+            },
+          }}
+        />
+        <Drawer.Screen
+          name="Preferences"
+          component={PreferencesScreen}
+          options={{
+            headerTitleStyle: {
+              fontSize: scale(13),
+              lineHeight: scale(13) * 0.75,
+              paddingTop: scale(13) - scale(13) * 0.75,
+            },
+            drawerLabelStyle: {
+              fontSize: scale(13),
+            },
+          }}
+        />
+        <Drawer.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            headerTitleStyle: {
+              fontSize: scale(13),
+              lineHeight: scale(13) * 0.75,
+              paddingTop: scale(13) - scale(13) * 0.75,
+            },
+            drawerLabelStyle: {
+              fontSize: scale(13),
+            },
+          }}
+        />
+        <Drawer.Screen
+          name="Logout"
+          component={LogoutScreen}
+          options={{
+            headerTitleStyle: {
+              fontSize: scale(13),
+              lineHeight: scale(13) * 0.75,
+              paddingTop: scale(13) - scale(13) * 0.75,
+            },
+            drawerLabelStyle: {
+              fontSize: scale(13),
+            },
+          }}
+        />
+      </Drawer.Navigator>
+    );
+  } else {
+    return (
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: {
+            height: scale(25),
+          },
+        }}>
+        <Drawer.Screen
+          name="Login"
+          component={login}
+          options={{
+            headerTitleStyle: {
+              fontSize: scale(13),
+              lineHeight: scale(13) * 0.75,
+              paddingTop: scale(13) - scale(13) * 0.75,
+            },
+            drawerLabelStyle: {
+              fontSize: scale(13),
+            },
+          }}
+        />
+        <Drawer.Screen
+          name="SignUp"
+          component={signup}
+          options={{
+            headerTitleStyle: {
+              fontSize: scale(13),
+              lineHeight: scale(13) * 0.75,
+              paddingTop: scale(13) - scale(13) * 0.75,
+            },
+            drawerLabelStyle: {
+              fontSize: scale(13),
+            },
+          }}
+        />
+      </Drawer.Navigator>
+    );
   }
-
-  return (
-    <Drawer.Navigator
-      // drawerContent={(props) => <CustomComponent {...props} />}
-      screenOptions={{
-        headerStyle: {
-          height: scale(25),
-        },
-      }}
-    >
-      <Drawer.Screen
-        name="Summary"
-        component={Tabstack}
-        options={{
-          headerTitleStyle: {
-            fontSize: scale(13),
-            lineHeight: scale(13) * 0.75,
-            paddingTop: scale(13) - scale(13) * 0.75,
-          },
-          drawerLabelStyle: {
-            fontSize: scale(13),
-          },
-          drawerItemStyle: { display: (logout===0)?"flex":"none", }
-        }}
-      />
-      <Drawer.Screen
-        name="Preferences"
-        component={PreferencesScreen}
-        options={{
-          headerTitleStyle: {
-            fontSize: scale(13),
-            lineHeight: scale(13) * 0.75,
-            paddingTop: scale(13) - scale(13) * 0.75,
-          },
-          drawerLabelStyle: {
-            fontSize: scale(13),
-          },
-          drawerItemStyle: { display: (logout===0)?"flex":"none", }
-        }}
-      />
-      <Drawer.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{
-          headerTitleStyle: {
-            fontSize: scale(13),
-            lineHeight: scale(13) * 0.75,
-            paddingTop: scale(13) - scale(13) * 0.75,
-          },
-          drawerLabelStyle: {
-            fontSize: scale(13),
-          },
-          drawerItemStyle: { display: (logout===0)?"flex":"none", }
-        }}
-      />
-      <Drawer.Screen
-        name="Login"
-        component={login}
-        options={{
-          headerTitleStyle: {
-            fontSize: scale(13),
-            lineHeight: scale(13) * 0.75,
-            paddingTop: scale(13) - scale(13) * 0.75,
-          },
-          drawerLabelStyle: {
-            fontSize: scale(13),
-          },
-          drawerItemStyle: { display: (logout===1)?"flex":"none", }
-        }}
-      />
-      <Drawer.Screen
-        name="Log out"
-        component={login}
-        options={{
-          headerTitleStyle: {
-            fontSize: scale(13),
-            lineHeight: scale(13) * 0.75,
-            paddingTop: scale(13) - scale(13) * 0.75,
-          },
-          drawerLabelStyle: {
-            fontSize: scale(13),
-          },
-          drawerItemStyle: { display: (logout===0)?"flex":"none", }
-        }}
-      />
-
-    </Drawer.Navigator>
-  );
 };
 
 export default Drawerstack;
