abf8174380d73fd8c55b1ea2ce66b77e122ffc82
Noel Miller <noeldamonmiller@gmail.com>
2023-06-19 14:46:09 -0700

    main removed hard-set firebase info

--- src/constants/firebase.ts
+++ src/constants/firebase.ts
@@ -2,13 +2,22 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSEGING_SENDER_ID,
+  APP_ID,
+} from 'react-native-dotenv';
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyALLOv-gv-cjOEq5gNDVuW2HZuV3EAb_zA',
-  authDomain: 'our-sponsors.firebaseapp.com',
-  projectId: 'our-sponsors',
-  storageBucket: 'our-sponsors.appspot.com',
-  messagingSenderId: '704163402159',
-  appId: '1:704163402159:web:887ecb34cd324c9b5e9f5a',
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSEGING_SENDER_ID,
+  appId: APP_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
@@ -18,22 +27,6 @@ const db = firebaseApp.firestore();
 export default Object.freeze({auth, db});
 export {auth, db};
 
-/*
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyALLOv-gv-cjOEq5gNDVuW2HZuV3EAb_zA",
-  authDomain: "our-sponsors.firebaseapp.com",
-  projectId: "our-sponsors",
-  storageBucket: "our-sponsors.appspot.com",
-  messagingSenderId: "704163402159",
-  appId: "1:704163402159:web:887ecb34cd324c9b5e9f5a"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-*/
